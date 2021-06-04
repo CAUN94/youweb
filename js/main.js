9ef7a619dd850982f48ac38c5737b3cf1d2d426b
caun94 <cristobalugarte6@gmail.com>
2021-06-04 12:25:48 -0400

    Jun 4 Jun YouWeb

--- js/main.js
+++ js/main.js
@@ -10,15 +10,15 @@ function shuffle(a) {
 }
 
 // Map
-var map = L.map('mapid').setView([-33.408578991937375, -70.577943164225], 14);
+var map = L.map('mapid').setView([-33.417642958010035, -70.57758338876975], 15);
 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-33.408578991937375, -70.577943164225]).addTo(map)
-    .bindPopup('You Just Better<br>Av. Apoquindo 4900,<br>Local 7')
+L.marker([-33.417642958010035, -70.57758338876975]).addTo(map)
+    .bindPopup('You Just Better<br>San Pascual 736, Las Condes')
     .openPopup();
 
 // Shuffle Image from Equipo
@@ -65,3 +65,4 @@ function w3RemoveClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
+
